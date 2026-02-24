a54e49292e34febef0ce69569af9a7428c0fc08c
fctorresmc-oss <fctorresmc@gmail.com>
2026-02-24 17:38:56 -0300

    Delete app directory

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{padding:40,fontFamily:"sans-serif"}}>
-      <h1>Arena Master</h1>
-      <p>Sistema de reservas funcionando ✅</p>
-    </main>
-  );
-}
